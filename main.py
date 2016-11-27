166a02bc5070cf7e1b3802339e99e1d76cc93ae2
Lev Tonkikh <leonst998@gmail.com>
2016-11-27 18:52:22 +0300

    generate_from_frequencies

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import csv
 
 from wordcloud import WordCloud
 
+tags = {}
+
 with open('favs/tags.csv', 'r') as f:
-    # WordCloud does not support generate from seq
-    # TODO generate_from_frequencies
-    text = ', '.join(row[0] for row in csv.reader(f))
+    for row in csv.reader(f):
+        tag = ''.join(row)
+        tags[tag] = 1 + tags.get(tag, 0)
 
-    print(text)
 
-    wordcloud = WordCloud(
-        width=2880,
-        height=1800,
-        scale=1,
-        font_path='/Library/Fonts/Verdana.ttf',
-        max_words=10000).generate(text)
-    wordcloud.to_file('test.png')
+wordcloud = WordCloud(
+    width=2880,
+    height=1800,
+    scale=1,
+    font_path='/Library/Fonts/Verdana.ttf',
+    max_words=10000).generate_from_frequencies(tags.items())
+
+wordcloud.to_file('test.png')
